7410a5de9da59ee690da14621d557fed9842694c
Dmytro from Linux <dimon200295@gmail.com>
2022-01-14 15:15:34 +0200

    modified index.php file

--- index.php
+++ index.php
@@ -10,3 +10,7 @@ echo "GitHub the best ;)";
 if (function_exist('writeLog')) {
 	writeLog('history.log');
 }
+
+if (random_int(1, 20) > 10 && file_exist("index-dev.php")) {
+	include "index-dev.php";
+}
